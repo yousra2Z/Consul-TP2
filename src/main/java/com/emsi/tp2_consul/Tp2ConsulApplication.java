664751e5a1056e0a8afffa0408009964bf3d6267
Yousra ZOUHRI <yousra@zone-tolearn.com>
2024-11-04 20:19:14 +0100

    Initial commit

---
+++ src/main/java/com/emsi/tp2_consul/Tp2ConsulApplication.java
@@ -0,0 +1,13 @@
+package com.emsi.tp2_consul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2ConsulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2ConsulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emsi/tp2_consul/Tp2ConsulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emsi.tp2_consul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2ConsulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
